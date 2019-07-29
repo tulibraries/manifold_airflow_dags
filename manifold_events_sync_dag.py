1267030e5db01da2f1b58b36ac377e3d2e43f023
Chad Nelson <chad.nelson@temple.edu>
2019-07-29 12:09:39 -0400

    Initial commit

---
+++ manifold_events_sync_dag.py
@@ -0,0 +1,58 @@
+"""Airflow DAG to Run the Events Sync rake task on a Manifold instance"""
+from datetime import datetime, timedelta
+import airflow
+from airflow.contrib.operators import SSHOperator
+from airflow.operators import PythonOperator
+from cob_datapipeline.task_slackpost import task_generic_slackpostsuccess
+
+MANIFOLD_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_INSTANCE")
+MANIFOLD_EVENTS_SYNC_INTERVAL = airflow.models.Variable.get("WEB_CONTENT_SCHEDULE_INTERVAL")
+#
+# CREATE DAG
+#
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'start_date': datetime(2019, 5, 28),
+    'email': ['chad.nelson@temple.edu'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+MANIFOLD_EVENTS_SYNC_DAG = airflow.DAG(
+    'manifold_events_sync', default_args=DEFAULT_ARGS, catchup=False,
+    max_active_runs=1, schedule_interval=MANIFOLD_EVENTS_SYNC_INTERVAL
+)
+
+#
+# CREATE TASKS
+#
+# Tasks with all logic contained in a single operator can be declared here.
+# Tasks with custom logic are relegated to individual Python files.
+#
+
+sync_events_bash = """
+sudo su - manifold bash -c \
+ "cd /var/www/manifold &&\
+ RAILS_ENV=production bundle exec rake sync:events"
+"""
+
+sync_events = SSHOperator(
+        task_id='sync_events',
+        command=sync_events_bash,
+        dag=MANIFOLD_EVENTS_SYNC_DAG,
+        ssh_conn_id='AIRFLOW_CONN_MANIFOLD_INSTANCE'
+    )
+
+post_slack = PythonOperator(
+    task_id='slack_post_succ',
+    python_callable=task_generic_slackpostsuccess,
+    provide_context=True,
+    dag=MANIFOLD_EVENTS_SYNC_DAG
+)
+
+#
+# SET UP TASK DEPENDENCIES
+#
+post_slack.set_upstream(sync_events)
