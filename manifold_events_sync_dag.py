1a125be6e43caa2cc35910296fecb506b53ac65e
Chad Nelson <chad.nelson@temple.edu>
2019-08-01 10:03:36 -0400

    WIP

--- manifold_events_sync_dag.py
+++ manifold_events_sync_dag.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import airflow
 from airflow.contrib.operators import SSHOperator
 from airflow.operators import PythonOperator
-from cob_datapipeline.task_slackpost import task_generic_slackpostsuccess
+from cob_datapipeline.task_slackpost import task_generic_slackpostsuccess, task_slackpostonfail
 
 MANIFOLD_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_INSTANCE")
 MANIFOLD_EVENTS_SYNC_INTERVAL = airflow.models.Variable.get("WEB_CONTENT_SCHEDULE_INTERVAL")
@@ -16,6 +16,7 @@ DEFAULT_ARGS = {
     'email': ['chad.nelson@temple.edu'],
     'email_on_failure': False,
     'email_on_retry': False,
+    'on_failure_callback': task_slackpostonfail,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
@@ -32,6 +33,19 @@ MANIFOLD_EVENTS_SYNC_DAG = airflow.DAG(
 # Tasks with custom logic are relegated to individual Python files.
 #
 
+
+def slackpostonsuccess(dag, **context):
+    """Task Method to Post Successful TUL Cob Index DAG Completion on Slack."""
+
+    ti = context.get('task_instance')
+    logurl = ti.log_url
+    dagid = ti.dag_id
+    date = context.get('execution_date')
+
+    message = "{} DAG {} success: Sync'd all the events {}".format(date, dagid, logurl)
+
+    return task_generic_slackpostsuccess(dag, message).execute(context=context)
+
 sync_events_bash = """
 sudo su - manifold bash -c \
  "cd /var/www/manifold &&\
@@ -47,7 +61,7 @@ sync_events = SSHOperator(
 
 post_slack = PythonOperator(
     task_id='slack_post_succ',
-    python_callable=task_generic_slackpostsuccess,
+    python_callable=slackpostonsuccess,
     provide_context=True,
     dag=MANIFOLD_EVENTS_SYNC_DAG
 )
