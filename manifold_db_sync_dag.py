87a2a78d55471e3f757059ce6bbbd92cb9dbd598
Chad Nelson <chad.nelson@temple.edu>
2019-08-12 18:21:49 -0400

    More working dags for db sync

--- manifold_db_sync_dag.py
+++ manifold_db_sync_dag.py
@@ -1,7 +1,8 @@
 """Airflow DAG to Sync data between Manifold instance DBs"""
 from datetime import datetime, timedelta
 import airflow
-from airflow.contrib.operators import SSHOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.operators.sftp_operator import SFTPOperator
 from airflow.operators import PythonOperator
 from cob_datapipeline.task_slackpost import task_generic_slackpostsuccess, task_slackpostonfail
 
@@ -9,10 +10,10 @@ MANIFOLD_PROD_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("A
 MANIFOLD_STAGE_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_STAGE_DB")
 MANIFOLD_QA_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_QA_DB")
 MANIFOLD_DB_SYNC_INTERVAL = airflow.models.Variable.get("MANIFOLD_DB_SYNC_SCHEDULE_INTERVAL")
-MANIFOLD_DB_DUMP_TIMESTAMP = datetime.now().replace(microsecond=0).isoformat()
+MANIFOLD_DB_DUMP_TIMESTAMP = airflow.models.Variable.get("MANIFOLD_DB_DUMP_TIMESTAMP")
 MANIFOLD_DB_DUMP_FILENAME = "manifold-prod-db-dump-{}.sqlc".format(MANIFOLD_DB_DUMP_TIMESTAMP)
 
-AF_DATA_DIR = Variable.get("AIRFLOW_DATA_DIR")
+AF_DATA_DIR = airflow.models.Variable.get("AIRFLOW_DATA_DIR")
 #
 # CREATE DAG
 #
@@ -22,7 +23,7 @@ DEFAULT_ARGS = {
     'email': ['chad.nelson@temple.edu'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'on_failure_callback': task_slackpostonfail,
+    #'on_failure_callback': task_slackpostonfail,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
@@ -47,24 +48,37 @@ def slackpostonsuccess(dag, **context):
     """Task Method to Post Successful TUL Cob Index DAG Completion on Slack."""
 
     ti = context.get('task_instance')
+    taskid = airflow.models.upstream_task_ids[0]
     logurl = ti.log_url
     dagid = ti.dag_id
     date = context.get('execution_date')
-    message = "{} DAG {} success: Sync'd all the Manifold DBs {}".format(date, dagid, logurl)
+    message = "{} DAG {} : TASK {} success: Sync'd\
+     all the Manifold DBs {}".format(date, dagid, taskid, logurl)
 
     return task_generic_slackpostsuccess(dag, message).execute(context=context)
 
 
 
-dump_prod_db_bash = "sudo su - pos bash -c \
-\"pg_dump -U dump --format=c --file=`/tmp/{}\" \
-manifold".format(MANIFOLD_DB_DUMP_FILENAME)
+dump_db_to_local_tmp_bash = "sudo su - postgres bash -c \
+\"pg_dump --format=c --file=/tmp/{} manifold\"".format(MANIFOLD_DB_DUMP_FILENAME)
 
+
+def set_timestamp(**kwargs):
+  ts = datetime.now().replace(microsecond=0).isoformat()
+  airflow.models.Variable.set("MANIFOLD_DB_DUMP_TIMESTAMP", ts)
+  return "MANIFOLD_DB_DUMP_TIMESTAMP set to {}".format(ts)
+
+
+set_dag_timestamp = PythonOperator(
+    task_id='set_dag_timestamp',
+    python_callable=set_timestamp,
+    dag=MANIFOLD_DB_SYNC_DAG
+)
 
 create_prod_db_dump = SSHOperator(
     task_id='manifold_create_prod_db_dump',
     command=dump_db_to_local_tmp_bash,
-    dag=MANIFOLD_EVENTS_SYNC_DAG,
+    dag=MANIFOLD_DB_SYNC_DAG,
     ssh_conn_id='AIRFLOW_CONN_MANIFOLD_PROD_DB'
 )
 
@@ -76,8 +90,7 @@ get_prod_db_dump = SFTPOperator(
     remote_filepath="/tmp/{}".format(MANIFOLD_DB_DUMP_FILENAME),
     operation="get",
     create_intermediate_dirs=True,
-    dag=dag
-)
+    dag=MANIFOLD_DB_SYNC_DAG)
 
 
 put_prod_dump_on_stage = SFTPOperator(
@@ -87,8 +100,7 @@ put_prod_dump_on_stage = SFTPOperator(
     remote_filepath="/tmp/{}".format(MANIFOLD_DB_DUMP_FILENAME),
     operation="put",
     create_intermediate_dirs=True,
-    dag=dag
-)
+    dag=MANIFOLD_DB_SYNC_DAG)
 
 
 put_prod_dump_on_qa = SFTPOperator(
@@ -98,49 +110,70 @@ put_prod_dump_on_qa = SFTPOperator(
     remote_filepath="/tmp/{}".format(MANIFOLD_DB_DUMP_FILENAME),
     operation="put",
     create_intermediate_dirs=True,
-    dag=dag
+    dag=MANIFOLD_DB_SYNC_DAG)
+
+
+restore_prod_dump_bash = "pg_restore -U restore -c  --dbname=manifold \
+/tmp/{}".format(MANIFOLD_DB_DUMP_FILENAME)
+
+restore_dump_on_stage = SSHOperator(
+    task_id='manifold_restore_dump_on_stage',
+    command=restore_prod_dump_bash,
+    dag=MANIFOLD_DB_SYNC_DAG,
+    ssh_conn_id='AIRFLOW_CONN_MANIFOLD_STAGE_DB')
+
+restore_dump_on_qa = SSHOperator(
+    task_id='manifold_restore_dump_on_qa',
+    command=restore_prod_dump_bash,
+    dag=MANIFOLD_DB_SYNC_DAG,
+    ssh_conn_id='AIRFLOW_CONN_MANIFOLD_QA_DB'
 )
 
 
-restore_prod_dump = "sudo su - postgres bash -c \
-\"pg_restore -U restore --format=c --file=`/tmp/{}\" \
-manifold".format(MANIFOLD_DB_DUMP_FILENAME)
-
-
-
- manifold_prod_s3_file_list = S3ListOperator(
-    task_id='list_prod_s3_files',
-    bucket='tulib-manifold-prod',
-    prefix='',
-    delimiter='/',
-    aws_conn_id='manifold_prod_s3_conn'
-)
-
- manifold_stage_s3_file_list = S3ListOperator(
-    task_id='list_stage_s3_files',
-    bucket='tulib-manifold-stage',
-    prefix='',
-    delimiter='/',
-    aws_conn_id='manifold_stage_s3_conn'
-)
-
- manifold_qa_s3_file_list = S3ListOperator(
-    task_id='list_qa_s3_files',
-    bucket='tulib-manifold-qa',
-    prefix='',
-    delimiter='/',
-    aws_conn_id='manifold_qa_s3_conn'
-)
+ # manifold_prod_s3_file_list = S3ListOperator(
+ #    task_id='list_prod_s3_files',
+ #    bucket='tulib-manifold-prod',
+ #    prefix='',
+ #    delimiter='/',
+ #    aws_conn_id='manifold_prod_s3_conn',
+ #    dag=MANIFOLD_EVENTS_SYNC_DAG)
+ #
+ # manifold_stage_s3_file_list = S3ListOperator(
+ #    task_id='list_stage_s3_files',
+ #    bucket='tulib-manifold-stage',
+ #    prefix='',
+ #    delimiter='/',
+ #    aws_conn_id='manifold_stage_s3_conn',
+ #    dag=MANIFOLD_EVENTS_SYNC_DAG)
+ #
+ # manifold_qa_s3_file_list = S3ListOperator(
+ #    task_id='list_qa_s3_files',
+ #    bucket='tulib-manifold-qa',
+ #    prefix='',
+ #    delimiter='/',
+ #    aws_conn_id='manifold_qa_s3_conn',
+ #    dag=MANIFOLD_EVENTS_SYNC_DAG)
 
 
 post_slack = PythonOperator(
     task_id='slack_post_succ',
     python_callable=slackpostonsuccess,
     provide_context=True,
-    dag=MANIFOLD_EVENTS_SYNC_DAG
-)
+    dag=MANIFOLD_DB_SYNC_DAG)
 
 #
 # SET UP TASK DEPENDENCIES
 #
-post_slack.set_upstream(sync_events)
+
+set_dag_timestamp.set_downstream(create_prod_db_dump)
+create_prod_db_dump.set_downstream(get_prod_db_dump)
+
+# STAGE OPERATIONS
+get_prod_db_dump.set_downstream(put_prod_dump_on_stage)
+put_prod_dump_on_stage.set_downstream(restore_dump_on_stage)
+restore_dump_on_stage.set_downstream(post_slack)
+
+#QA OPERATIONS
+get_prod_db_dump.set_downstream(put_prod_dump_on_qa)
+put_prod_dump_on_qa.set_downstream(restore_dump_on_qa)
+restore_dump_on_qa.set_downstream(post_slack)
