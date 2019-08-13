7dc00aab4221f2c83fea63dab78987d97b80fe5e
Chad Nelson <chad.nelson@temple.edu>
2019-08-13 07:58:54 -0400

    Add Blogs and Hours sync

--- manifold_hours_sync_dag.py
+++ manifold_hours_sync_dag.py
@@ -1,4 +1,4 @@
-"""Airflow DAG to Run the Events Sync rake task on a Manifold instance"""
+"""Airflow DAG to Run the Blogs Sync rake task on a Manifold instance"""
 from datetime import datetime, timedelta
 import airflow
 from airflow.contrib.operators import SSHOperator
@@ -6,7 +6,7 @@ from airflow.operators import PythonOperator
 from cob_datapipeline.task_slackpost import task_generic_slackpostsuccess, task_slackpostonfail
 
 MANIFOLD_INSTANCE_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_INSTANCE")
-MANIFOLD_EVENTS_SYNC_INTERVAL = airflow.models.Variable.get("MANIFOLD_EVENTS_SYNC_SCHEDULE_INTERVAL")
+MANIFOLD_HOURS_SYNC_INTERVAL = airflow.models.Variable.get("MANIFOLD_HOURS_SYNC_SCHEDULE_INTERVAL")
 #
 # CREATE DAG
 #
@@ -21,9 +21,9 @@ DEFAULT_ARGS = {
     'retry_delay': timedelta(minutes=5),
 }
 
-MANIFOLD_EVENTS_SYNC_DAG = airflow.DAG(
-    'manifold_events_sync', default_args=DEFAULT_ARGS, catchup=False,
-    max_active_runs=1, schedule_interval=MANIFOLD_EVENTS_SYNC_INTERVAL
+MANIFOLD_HOURS_SYNC_DAG = airflow.DAG(
+    'manifold_hours_sync', default_args=DEFAULT_ARGS, catchup=False,
+    max_active_runs=1, schedule_interval=MANIFOLD_HOURS_SYNC_INTERVAL
 )
 
 #
@@ -42,20 +42,20 @@ def slackpostonsuccess(dag, **context):
     dagid = ti.dag_id
     date = context.get('execution_date')
 
-    message = "{} DAG {} success: Sync'd all the events {}".format(date, dagid, logurl)
+    message = "{} DAG {} success: Sync'd all the hours {}".format(date, dagid, logurl)
 
     return task_generic_slackpostsuccess(dag, message).execute(context=context)
 
-sync_events_bash = """
+sync_hours_bash = """
 sudo su - manifold bash -c \
  "cd /var/www/manifold &&\
- RAILS_ENV=production bundle exec rake sync:events"
+ RAILS_ENV=production bundle exec rake sync:hours"
 """
 
-sync_events = SSHOperator(
-        task_id='sync_events',
-        command=sync_events_bash,
-        dag=MANIFOLD_EVENTS_SYNC_DAG,
+sync_hours = SSHOperator(
+        task_id='sync_hours',
+        command=sync_hours_bash,
+        dag=MANIFOLD_HOURS_SYNC_DAG,
         ssh_conn_id='AIRFLOW_CONN_MANIFOLD_INSTANCE'
     )
 
@@ -63,10 +63,10 @@ post_slack = PythonOperator(
     task_id='slack_post_succ',
     python_callable=slackpostonsuccess,
     provide_context=True,
-    dag=MANIFOLD_EVENTS_SYNC_DAG
+    dag=MANIFOLD_HOURS_SYNC_DAG
 )
 
 #
 # SET UP TASK DEPENDENCIES
 #
-post_slack.set_upstream(sync_events)
+post_slack.set_upstream(sync_hours)
