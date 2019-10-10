416df6eb2fb73cf0240da34bc0f933ed17f1aafd
Chad Nelson <chad.nelson@temple.edu>
2019-10-10 16:50:20 -0400

    Add dag to run sitemap generator

---
+++ manifold_generate_sitemap_dag.py
@@ -0,0 +1,72 @@
+"""Airflow DAG to Run the Sitemap Create rake task on a Manifold instance"""
+from datetime import datetime, timedelta
+import airflow
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.python_operator import PythonOperator
+from tulflow import tasks
+
+MANIFOLD_INSTANCE_SSH_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_MANIFOLD_SSH_INSTANCE")
+MANIFOLD_GENERATE_SITEMAP_INTERVAL = airflow.models.Variable.get("MANIFOLD_GENERATE_SITEMAP_SCHEDULE_INTERVAL", default_var="@weekly")
+#
+# CREATE DAG
+#
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'start_date': datetime(2019, 5, 28),
+    'email': ['chad.nelson@temple.edu'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': tasks.execute_slackpostonfail,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+MANIFOLD_GENERATE_SITEMAP_DAG = airflow.DAG(
+    'manifold_generate_sitemap', default_args=DEFAULT_ARGS, catchup=False,
+    max_active_runs=1, schedule_interval=MANIFOLD_GENERATE_SITEMAP_INTERVAL
+)
+
+#
+# CREATE TASKS
+#
+# Tasks with all logic contained in a single operator can be declared here.
+# Tasks with custom logic are relegated to individual Python files.
+#
+
+
+def slackpostonsuccess(dag, **context):
+    """Task Method to Post Successful Generate Sitemap DAG Completion on Slack."""
+
+    ti = context.get('task_instance')
+    logurl = ti.log_url
+    dagid = ti.dag_id
+    date = context.get('execution_date')
+
+    message = "{} DAG {} success: Sitemap successfully generated {}".format(date, dagid, logurl)
+
+    return tasks.slackpostonsuccess(dag, message).execute(context=context)
+
+generate_sitemap_bash = """
+sudo su - manifold bash -c \
+ "cd /var/www/manifold &&\
+ RAILS_ENV=production bundle exec rake sitemap:create"
+"""
+
+generate_sitemap = SSHOperator(
+        task_id='generate_sitemap',
+        command=generate_sitemap_bash,
+        dag=MANIFOLD_GENERATE_SITEMAP_DAG,
+        ssh_conn_id='AIRFLOW_CONN_MANIFOLD_SSH_INSTANCE'
+    )
+
+post_slack = PythonOperator(
+    task_id='slack_post_succ',
+    python_callable=slackpostonsuccess,
+    provide_context=True,
+    dag=MANIFOLD_GENERATE_SITEMAP_DAG
+)
+
+#
+# SET UP TASK DEPENDENCIES
+#
+post_slack.set_upstream(generate_sitemap)
