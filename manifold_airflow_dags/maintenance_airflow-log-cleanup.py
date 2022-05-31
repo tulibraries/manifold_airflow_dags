73bc7f78cbb9c066da53af0fe261c5a03433a240
sensei100 <tuh42082@temple.edu>
2022-05-31 11:50:28 -0400

    Update maintenance dags for log settings

--- manifold_airflow_dags/maintenance_airflow-log-cleanup.py
+++ manifold_airflow_dags/maintenance_airflow-log-cleanup.py
@@ -19,7 +19,7 @@ import airflow
 # airflow-log-cleanup
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 START_DATE = airflow.utils.dates.days_ago(1)
-BASE_LOG_FOLDER = conf.get("core", "BASE_LOG_FOLDER")
+BASE_LOG_FOLDER = conf.get("logging", "BASE_LOG_FOLDER")
 # How often to Run. @daily - Once a day at Midnight
 SCHEDULE_INTERVAL = "@daily"
 # Who is listed as the owner of this DAG in the Airflow Web Server
@@ -67,7 +67,7 @@ logging.info("ENABLE_DELETE_CHILD_LOG  " + ENABLE_DELETE_CHILD_LOG)
 if not BASE_LOG_FOLDER or BASE_LOG_FOLDER.strip() == "":
     raise ValueError(
         "BASE_LOG_FOLDER variable is empty in airflow.cfg. It can be found "
-        "under the [core] section in the cfg file. Kindly provide an "
+        "under the [logging] section in the cfg file. Kindly provide an "
         "appropriate directory path."
     )
 
