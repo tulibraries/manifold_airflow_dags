597a4ec9d65d1de7f188fbc2d247260fbcbee7dd
Chris Doyle <cdoyle@temple.edu>
2023-10-02 08:40:17 -0400

    add f to f-string

--- manifold_airflow_dags/manifold_database_sync_dag.py
+++ manifold_airflow_dags/manifold_database_sync_dag.py
@@ -76,7 +76,7 @@ POST_SLACK = PythonOperator(
     dag=DAG)
 
 # Create a temporary SQL script to Terminate connections to Manifold DB so we can drop the DB.
-disconnect_db_sql = """
+disconnect_db_sql = f"""
   sudo su - postgres bash -c 'echo "select pg_terminate_backend(pid) from pg_stat_activity where datname=\'manifold\' AND application_name!=\'psql\';" > /tmp/disconnect_db.sql'
 """
 
